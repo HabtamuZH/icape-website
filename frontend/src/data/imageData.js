f85e424d974abd8038a623857b1ad09043005f15
Girum-Tamirat <girumtamirat10@gmail.com>
2025-02-12 10:05:29 +0300

    refactor: move project images to separate data file for better organization

---
+++ frontend/src/data/imageData.js
@@ -0,0 +1,33 @@
+import image1 from "/images/image1.jpg";
+import image2 from "/images/image2.jpg";
+import image3 from "/images/image3.jpg";
+import image4 from "/images/image4.jpg";
+import image5 from "/images/image5.jpg";
+import image6 from "/images/image6.jpg";
+import image7 from "/images/image7.jpg";
+import image8 from "/images/image8.jpg";
+import image9 from "/images/image9.jpg";
+import image10 from "/images/image10.jpg";
+import image11 from "/images/image11.jpg";
+import image12 from "/images/image12.jpg";
+import image13 from "/images/image13.jpg";
+import image14 from "/images/image14.jpg";
+import image15 from "/images/image15.jpg";
+import image16 from "/images/image16.jpg";
+import image17 from "/images/image17.jpg";
+import image18 from "/images/image18.jpg";
+import image19 from "/images/image19.jpg";
+import image20 from "/images/image20.jpg";
+import image21 from "/images/image21.jpg";
+import icapeLogo from "/images/icape_Logo.png";
+
+const projectImages = {
+  apartment: [image1, image2, image3, image16, image17],
+  cbeBank: [image4, image5, image6, image18, image19],
+  jcc: [image7, image8, image9, image20, image21],
+  garaMart: [image10, image11, image12, image16, image17],
+  waritZ: [image13, image14, image15, image18, image19],
+};
+
+export default projectImages;
+export { icapeLogo };
