f033272f3fee7f897bb042336cee5cd96e9659e4
Esubale Endale <esu1011esu@gmail.com>
2025-03-07 14:37:37 +0300

    refactor: restructure service descriptions and consolidate service files

---
+++ frontend/src/data/services/designDepartment.js
@@ -0,0 +1,25 @@
+
+const architecturalDesignDescription = [
+  {
+    subTitle: "Aesthetic & Functional Design",
+    content:
+      "At the core of our architectural design is a deep understanding of aesthetics and functionality, ensuring that every structure is both beautiful and practical.",
+  },
+  {
+    subTitle: "Innovative & Sustainable Solutions",
+    content:
+      "We blend cutting-edge design principles with sustainable technologies to create environmentally responsible and future-ready buildings.",
+  },
+  {
+    subTitle: "Customization & Personalization",
+    content:
+      "Each project is tailored to the unique needs of our clients, reflecting their vision while integrating modern architectural trends.",
+  },
+  {
+    subTitle: "Timeless & Enduring Structures",
+    content:
+      "Our focus is on crafting spaces that stand the test of time, combining durability, efficiency, and contemporary design elements.",
+  },
+];
+
+export default architecturalDesignDescription;
---
+++ frontend/src/data/services/services.js
@@ -0,0 +1,26 @@
+import DesignIcon from "../../../public/icon/design.png";
+import urban from "../../../public/icon/urban.png";
+import landscape from "../../../public/icon/landscape.png";
+
+const services = [
+  {
+    title: "Architectural Design, Design Department",
+    description:
+      "Innovative and sustainable design solutions for modern architecture.",
+    icon: DesignIcon, // Matches: design icon for architectural design
+  },
+  {
+    title: "Urban Design And Planning",
+    description:
+      "Efficient and forward-thinking urban planning services for thriving communities.",
+    icon: urban, // Matches: urban icon for urban design
+  },
+  {
+    title: "Contract Administration and Engineering Design",
+    description:
+      "Delivering projects with precision through expert contract management and engineering design.",
+    icon: landscape,
+  },
+];
+
+export default services;
