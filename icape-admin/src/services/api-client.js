70fb9d530a3ff8901de07efe05f72d57a727788e
HabtamuZH <habtamuzenebe42@gmail.com>
2025-03-13 09:41:39 +0300

    Api changed

--- icape-admin/src/services/api-client.js
+++ icape-admin/src/services/api-client.js
@@ -1,8 +1,8 @@
 import axios, { CanceledError } from "axios";
 
-// const baseUrl = "https://api.icape.studio/";
+const baseUrl = "https://api.icape.studio/";
 // const baseUrl = "http://localhost:5001/";
-const baseUrl = "https://5001-idx-icape-website-1741507084977.cluster-rz2e7e5f5ff7owzufqhsecxujc.cloudworkstations.dev/";
+// const baseUrl = "https://5001-idx-icape-website-1741507084977.cluster-rz2e7e5f5ff7owzufqhsecxujc.cloudworkstations.dev/";
 
 const apiClient = axios.create({
   baseURL: baseUrl,
