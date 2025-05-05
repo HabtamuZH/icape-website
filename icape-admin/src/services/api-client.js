159e69a5fbe09242228046f3e759216d6f60aa67
HabtamuZH <habtamuzenebe42@gmail.com>
2025-05-05 14:10:19 +0300

    Update API base URL to production endpoint

--- icape-admin/src/services/api-client.js
+++ icape-admin/src/services/api-client.js
@@ -1,7 +1,6 @@
 import axios, { CanceledError } from "axios";
 
-// const baseUrl = "https://api.icape.studio/";
-const baseUrl = "http://localhost:5001/";
+const baseUrl = "https://api.icape.studio/";
 // const baseUrl = "https://5001-idx-icape-website-1741507084977.cluster-rz2e7e5f5ff7owzufqhsecxujc.cloudworkstations.dev/";
 
 const apiClient = axios.create({
@@ -10,5 +9,3 @@ const apiClient = axios.create({
 
 export default apiClient;
 export { CanceledError, baseUrl };
-
-// const baseUrl = "https://5001-idx-icape-websitegit-1738576899242.cluster-23wp6v3w4jhzmwncf7crloq3kw.cloudworkstations.dev/";
