e421b000ba9d7372a822457d6f53c01d5163938c
HabtamuZH <habtamuzenebe42@gmail.com>
2025-05-10 16:46:06 +0300

    Refactor: Remove unused components and hooks related to team management and applications
    - Deleted UpdateProjectForm component as it is no longer needed.
    - Removed AdminTeamManagement, ConfirmDeleteModal, TeamForm, TeamList components.
    - Eliminated optionData.js, useApplications.js, useFeedbacks.js, and useProfile.js hooks.
    - Cleaned up imports and adjusted related files accordingly.

--- frontend/src/admin/components/feedback/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/prop-types */
-
-const ErrorMessage = ({ message }) => {
-  return <div className="p-6 text-red-500 font-body">{message}</div>;
-};
-
-export default ErrorMessage;
--- icape-admin/src/admin/components/blogs/BlogCard.jsx
+++ icape-admin/src/admin/components/blogs/BlogCard.jsx
@@ -1,6 +1,6 @@
 // src/components/Blog/BlogCard.js
 import React from "react";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
 const BlogCard = ({ blog, onUpdate, onDelete }) => {
